8aa695c3f6185b55d91d3da08ab238e023f2cf74
James Burke <james.walter.burke@gmail.com>
2020-10-13 12:42:01 -0400

    Bump plugin version definition

--- fb-ig-embeds-audit.php
+++ fb-ig-embeds-audit.php
@@ -12,7 +12,7 @@
 namespace FB_IG_Embeds_Audit;
 
 define( 'FB_IG_EMBEDS_AUDIT_PATH', dirname( __FILE__ ) );
-define( 'FB_IG_EMBEDS_AUDIT_VERSION', '1.0.0' );
+define( 'FB_IG_EMBEDS_AUDIT_VERSION', '1.0.1' );
 
 // Load traits.
 require_once FB_IG_EMBEDS_AUDIT_PATH . '/inc/trait-cli-bulk-task.php';
